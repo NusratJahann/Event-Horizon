669e676831ca1ece60cbc2f6efaa51d26c7554ee
NusratJahann <dilerakhatun52@gmail.com>
2023-05-15 21:08:08 +0600

    login ui and loading screen

---
+++ src/components/Shared/Loading/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <div class="flex items-center justify-center h-screen">
+        <div class="w-16 h-16 border-l-2 border-accent rounded-full animate-spin"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
